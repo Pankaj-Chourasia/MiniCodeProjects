6f54258cb9df3074cdac81555617f2c451304e91
Pankaj Chourasia <pankaj.chourasia.sbg@gmail.com>
2024-03-16 21:44:15 +0530

    add 50 minute time-interval

--- pomodoro-timer/script.js
+++ pomodoro-timer/script.js
@@ -2,27 +2,56 @@ let timer;
 let minutes = 25;
 let seconds = 0;
 let isTimerRunning = false;
+let pausedTime = null;
 
 const startBtn = document.getElementById('start-btn');
 const pauseBtn = document.getElementById('pause-btn');
 const resetBtn = document.getElementById('reset-btn');
 const timerMinutes = document.getElementById('timer-minutes');
 const timerSeconds = document.getElementById('timer-seconds');
+const interval25Radio = document.getElementById('interval-25');
+const interval50Radio = document.getElementById('interval-50');
+
 
 function startTimer() {
-    isTimerRunning = true;
-    timer = setInterval(updateTimer, 1000);
+    if (!isTimerRunning) {
+        if (interval50Radio.checked) {
+            minutes = 50;
+        } else {
+            minutes = 25;
+        }
+        
+          // If timer was paused, resume countdown from stored time
+          if (pausedTime) {
+            minutes = pausedTime.minutes;
+            seconds = pausedTime.seconds;
+            pausedTime = null; 
+        }
+
+        isTimerRunning = true;
+        timer = setInterval(updateTimer, 1000);
+    }
 }
 
 function pauseTimer() {
     isTimerRunning = false;
     clearInterval(timer);
+
+    pausedTime = {
+        minutes: minutes,
+        seconds: seconds
+    };
 }
 
 function resetTimer() {
     isTimerRunning = false;
     clearInterval(timer);
-    minutes = 25;
+    
+    if (interval50Radio.checked) {
+        minutes = 50;
+    } else {
+        minutes = 25;
+    }
     seconds = 0;
     updateTimerDisplay();
 }
@@ -48,9 +77,26 @@ function updateTimerDisplay() {
     timerSeconds.textContent = padTime(seconds);
 }
 
+function setTimerDuration() {
+    if(interval50Radio.checked){
+        minutes = 50;
+    }else {
+        minutes = 25;
+    }
+
+    seconds = 0;
+    updateTimerDisplay();
+}
+
 function padTime(time) {
     return (time < 10 ? '0' : '') + time;
 }
+
+interval25Radio.addEventListener('change', setTimerDuration);
+interval50Radio.addEventListener('change', setTimerDuration);
+
+
+setTimerDuration();
 
 startBtn.addEventListener('click', () => {
     if (!isTimerRunning) {
