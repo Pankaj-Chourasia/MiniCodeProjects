0ce539acdd9c326f8bd077aa09244e0eff15c067
Pankaj Chourasia <pankaj.chourasia.sbg@gmail.com>
2024-04-05 15:37:56 +0530

    fix issues & update dropdown.jsx
    add swap button and design handlers

--- currency-converter/src/components/currency-converter.jsx
+++ currency-converter/src/components/currency-converter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import CurrencyDropdown from './dropdown';
+import { HiArrowsRightLeft } from 'react-icons/hi2';
 
 const CurrencyConverter = () => {
     const [currencies, setCurrencies] = useState([]);
@@ -16,7 +17,7 @@ const CurrencyConverter = () => {
         try {
             const res = await fetch(`https://api.frankfurter.app/currencies`);
             const data = await res.json();
-            setCurrencies(data);
+            setCurrencies(Object.keys(data));
         } catch (error) {
             console.log("Error fetching :", error);
         }
@@ -30,17 +31,37 @@ const CurrencyConverter = () => {
     const convertCurrency = () => {
 
     }
+
+    const handleFavoriteCurrencies = (currentCurrencies) => {
+        
+    }
     //! Conversion -> https://api.frankfurter.app/latest?amount=1&from=USD&to=INR
 
   return (
     <div className='max-w-xl mx-auto my-10 p-5 bg-white rounded-lg shadow-md'>
         <h2 className='mb-5 text-2xl font-semibold text-gray-700'>Currency Converter</h2>
 
-        <div>
-            <CurrencyDropdown/>
-
-
-            <CurrencyDropdown/>
+        <div className='grid grid-cols-1 sm:grid-cols-3 gap-4 items-end'>
+        <CurrencyDropdown 
+                    
+                    allCurrencies={currencies}
+                    title="From"
+                    currentCurrencies={fromCurrency}
+                    setCurrencies={setFromCurrency}
+                    handleFavoriteCurrencies={handleFavoriteCurrencies}
+                />
+            <div className='flex justify-center -mb-5 sm:mb-0'>
+                <button onClick={swapCurrencies} className='p-2 bg-gray-200 rounded-full cursor-pointer hover:bg-gray-300'>
+                    <HiArrowsRightLeft className='text-xl text-gray-700'/>
+                </button>
+            </div>
+            <CurrencyDropdown
+                    allCurrencies={currencies}
+                    currentCurrencies={toCurrency}
+                    setCurrencies={setToCurrency}
+                    title="To: "
+                    handleFavoriteCurrencies={handleFavoriteCurrencies}
+                />
         </div>
 
         <div className='mt-4'>
@@ -50,7 +71,7 @@ const CurrencyConverter = () => {
             <input
             value={amount} 
             onChange={(e) => setAmount(e.target.value)}
-            className='w-full border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 mt-1'
+            className='w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 mt-1'
             type="number"/>
         </div>
 
--- currency-converter/src/components/dropdown.jsx
+++ currency-converter/src/components/dropdown.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React from 'react';
+import {HiOutlineStar, HiStar} from "react-icons/hi2";
 
-const CurrencyDropdown = (
+const CurrencyDropdown = ({
     allCurrencies,
     currentCurrencies,
     setCurrencies,
     favoriteCurrencies,
     handleFavoriteCurrencies,
     title = "",
-) => {
+}) => {
   return (
     <div>
-        <label htmlFor= {title}>{title}</label>
+        <label 
+          htmlFor= {title}
+          className='block text-sm font-medium text-gray-700'>
+            {title}
+            </label>
+    <div className='mt-1 relative'>
+        <select value={currentCurrencies} onChange = {(e)=>setCurrencies(e.target.value)} className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 bg-gray-200">
+          {/* render favoriteCurrencies */}
+          <hr />
+          {allCurrencies.map((currentCurrencies) => {
+            return (
+              <option value={currentCurrencies} key={currentCurrencies}>
+              {currentCurrencies}
+            </option>
+            );
+          })}
+        </select>
+
+        <button 
+        onClick={()=>handleFavoriteCurrencies(currentCurrencies)}
+        className="absolute inset-y-0 right-0 pr-5 flex items-center text-sm leading-5">
+          <HiOutlineStar />
+        </button>
+    </div>
     </div>
   )
 }
